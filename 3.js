872c397bc27310638e510ea380a6104359df846b
Mr.Worldwide <syahfisal37@yahoo.com>
2019-02-21 11:58:31 +0800

    finalize test

--- 3.js
+++ 3.js
@@ -1,4 +1,10 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const FlashMessage = styled.div`
+  background: ${props => props.primary ? "#d8f5d7" : "#f5dbdc"};
+  color: ${props => props.primary ? "#1e7f40" : "#b2353d"};
+`;
 export default class Form extends React.PureComponent {
 
     constructor () {
@@ -10,9 +16,11 @@ export default class Form extends React.PureComponent {
         birthDate: '',
         pJobTitle: '',
         yearExp: '',
+        isSuccessSubmission: false,
+        isHasFormRespond :false,
+        respondMsg:''
       };
     }
-    
     handleChange = (evt) => {
       // check it out: we get the evt.target.name (which will be either "fName", "lName" and so on)
       // and use it to target the key on our `state` object with the same name, using bracket syntax
@@ -34,16 +42,42 @@ export default class Form extends React.PureComponent {
         console.log(this.state)
         if(this.validateEmail(this.state.email) == true){
             console.log('valid email');
-            console.log(this.validateName(`${this.state.fName} ${this.state.lName}`)); 
+            console.log(this.validateName(`${this.state.fName} ${this.state.lName}`));
+            if(this.validateName(`${this.state.fName} ${this.state.lName}`) == true){
+              this.onSubmitData(this.state)
+            }
         } else{
             console.log('not valid');
         }
     }
     
+    onSubmitData = (formData) => {
+      const that = this; // I usually use redux to manage calling APIs action and saga for request handling
+      fetch('https://api.dummyendpoint/me/profile ', {
+        method:'PUT',
+        body: formData  // or JSON.stringify(data)
+        // headers
+        //credentials
+      })
+      .then(function(data) {
+        that.setState({isHasFormRespond:true, isSuccessSubmission: true, respondMsg:'Success'})
+        // Here to be done when onSuccess.
+        // Probably show or navigate other page.
+        // can use data.json() to obtain JSON
+      }).catch(function(error) {
+        that.setState({isHasFormRespond:true, isSuccessSubmission:false, respondMsg:'Error'})
+        // Here to be done when onError.
+        // Probably you want to highlight invalid. maybe danger border colour at field not satisfy
+      }); 
+    }
     render () {
+      const {isSuccessSubmission, isHasFormRespond, respondMsg} = this.state;
       return (
         <form onSubmit={this.handleSubmit}>
-        
+          { isHasFormRespond && <FlashMessage successness={ isSuccessSubmission}>
+              {respondMsg}
+            </FlashMessage>
+          }
           <label>First name</label>
           <input type="text" name="fName" onChange={this.handleChange} />
           
@@ -66,4 +100,4 @@ export default class Form extends React.PureComponent {
         </form>
       );
     }
-  }
+}
